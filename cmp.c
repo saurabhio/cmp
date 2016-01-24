06a6dbf77e4ef58564dd5125a107e0edbd2f8fe7
Saurabh <saurabhio@users.noreply.github.com>
2016-01-24 09:16:42 +0530

    Update cmp.c

--- cmp.c
+++ cmp.c
@@ -1,47 +1,51 @@
 /**
  * This is a function for comparing two files.
  * DEVELOPER   : SAURABH
- * LAST UPDATE : 28-JUL-2014
+ * LAST UPDATE : 24-JAN-2016
  */
-
- void cmp(void)
+ 
+void cmp(void)
 {
 	FILE *fp1, *fp2;
 	int ca, cb;
 	char fname1[100], fname2[100] ;
-
-  printf("(eg. C:/.../file)\n\n");
+	
+	printf("(eg. C:/.../file)\n\n");
 	printf("Path of first file  :  ");
 	gets(fname1);
-	fp1 = fopen( fname1, "r" );
+	
+	fp1 = fopen (fname1, "r");
 
-	if ( fp1 == NULL )
+	if (fp1 == NULL)
 	{
-		printf("\n\tUnable to open \"%s\" file.\n",fname1);
+		printf("\n\tUnable to open \"%s\" file.\n", fname1);
 	}
 	else
-    {	  printf("Path of second file :  ");
-        gets(fname2);
-	      fp2 = fopen( fname2, "r" ) ;
+	{
+		printf("Path of second file :  ");
+		gets(fname2);
+		fp2 = fopen (fname2, "r") ;
 
-  		if ( fp2 == NULL )
+  		if (fp2 == NULL)
   		{
-  			printf("\n\tUnable to open \"%s\" file.\n",fname2);
+  			printf("\n\tUnable to open \"%s\" file.\n", fname2);
   		}
   		else
   		{
-  			do{ ca = getc( fp1 ) ;
-  				  cb = getc( fp2 ) ;
-  			  }while ( ca != EOF && cb != EOF && ca == cb );
+  			do
+  			{
+  				ca = getc (fp1) ;
+  				cb = getc (fp2);
+  				
+  			}while (ca != EOF && cb != EOF && ca == cb);
   
-  			if ( ca == cb )
+  			if (ca == cb)
   				printf("\n\t\tIDENTICAL Files.\n");
-  			else if ( ca != cb )
-  				printf("\n\t\tDIFFERENT Files.\n" );
+  			else if (ca != cb)
+  				printf("\n\t\tDIFFERENT Files.\n");
   
-  			fclose ( fp2 );
+  			fclose (fp2);
   		}
     }
-	
-	fclose ( fp1 );
+    fclose (fp1);
 }
